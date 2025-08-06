bfbcc2cff4d61e4f0c62532d7cfed79b5de027f2
sahanareddy18 <sahanareddykanukuntla18@gmail.com>
2025-08-06 13:19:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ 
-  origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000",'https://psmabs.onrender.com'],
   credentials: true 
 }));
 app.use(express.json());
