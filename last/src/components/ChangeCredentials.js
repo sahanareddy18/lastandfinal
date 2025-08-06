a4f50ae16d6ad6fa4ec3a0753600e7286bcf03c1
sahanareddy18 <sahanareddykanukuntla18@gmail.com>
2025-08-06 12:36:15 +0530

    Update ChangeCredentials.js

--- last/src/components/ChangeCredentials.js
+++ last/src/components/ChangeCredentials.js
@@ -36,7 +36,7 @@ const ChangeCredentials = () => {
     setMessage('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/change-password', {
+      const response = await axios.post('https://pharmacystockmanagmentandbillingsystemba.onrender.com/api/auth/change-password', {
         email: email,
         currentPassword: '', // Not needed for admin reset
         newPassword: newPassword
